cfd7196daa8ba64fdedb4cd3f8b1a2745b1b3968
Jose Lestrades <43249222+lestradess@users.noreply.github.com>
2022-07-16 19:28:54 +0200

    Inicio de sesión con FirebaseUI --Funcionando--

--- app/src/main/java/com/lestrades/firebasevendedores/MainActivity.kt
+++ app/src/main/java/com/lestrades/firebasevendedores/MainActivity.kt
@@ -2,10 +2,34 @@ package com.lestrades.firebasevendedores
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val providers = arrayListOf(AuthUI.IdpConfig.EmailBuilder().build())
+
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            val response = IdpResponse.fromResultIntent(it.data)
+
+            if (it.resultCode == RESULT_OK) {
+                val user = FirebaseAuth.getInstance().currentUser
+                if (user != null) {
+                    Toast.makeText(this, "Bienvenido", Toast.LENGTH_LONG).show()
+                }
+            }
+        }.launch(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build()
+        )
     }
 }
