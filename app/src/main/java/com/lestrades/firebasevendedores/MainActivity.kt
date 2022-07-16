90660b7521078524268dee5f3d95166fdbceba0a
Jose Lestrades <43249222+lestradess@users.noreply.github.com>
2022-07-16 19:39:57 +0200

    Mantener la sesión activa para no terner que logearse cada vez que se entra.

--- app/src/main/java/com/lestrades/firebasevendedores/MainActivity.kt
+++ app/src/main/java/com/lestrades/firebasevendedores/MainActivity.kt
@@ -10,26 +10,51 @@ import com.google.firebase.auth.FirebaseAuth
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var authStateListener: FirebaseAuth.AuthStateListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val providers = arrayListOf(AuthUI.IdpConfig.EmailBuilder().build())
+        configAuth()
+    }
 
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-            val response = IdpResponse.fromResultIntent(it.data)
+    private fun configAuth(){
+        firebaseAuth = FirebaseAuth.getInstance()
+        authStateListener = FirebaseAuth.AuthStateListener {
+            if (it.currentUser != null){
+                supportActionBar?.title = it.currentUser?.displayName
+            }else{
+                val providers = arrayListOf(AuthUI.IdpConfig.EmailBuilder().build())
 
-            if (it.resultCode == RESULT_OK) {
-                val user = FirebaseAuth.getInstance().currentUser
-                if (user != null) {
-                    Toast.makeText(this, "Bienvenido", Toast.LENGTH_LONG).show()
-                }
+                registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                    val response = IdpResponse.fromResultIntent(it.data)
+
+                    if (it.resultCode == RESULT_OK) {
+                        val user = FirebaseAuth.getInstance().currentUser
+                        if (user != null) {
+                            Toast.makeText(this, "Bienvenido", Toast.LENGTH_LONG).show()
+                        }
+                    }
+                }.launch(
+                    AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build()
+                )
             }
-        }.launch(
-            AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(providers)
-                .build()
-        )
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        firebaseAuth.addAuthStateListener (authStateListener)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        firebaseAuth.removeAuthStateListener (authStateListener)
     }
 }
