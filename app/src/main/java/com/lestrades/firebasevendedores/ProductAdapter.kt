f7f05dded23a37f4889b2bf3ed0972ec14e72782
Jose Lestrades <43249222+lestradess@users.noreply.github.com>
2022-07-24 12:07:37 +0200

    Adaptador de productos

--- app/src/main/java/com/lestrades/firebasevendedores/ProductAdapter.kt
+++ app/src/main/java/com/lestrades/firebasevendedores/ProductAdapter.kt
@@ -1,13 +1,44 @@
 package com.lestrades.firebasevendedores
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.lestrades.firebasevendedores.databinding.ItemProductBinding
 
 class ProductAdapter(
     private val productList: MutableList<Product>,
     private val listener: OnProductLisener
-) {
+) :
+    RecyclerView.Adapter<ProductAdapter.ViewHolder>() {
+
+    private lateinit var context: Context
+    //Ser crea la vista
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        context = parent.context
+        val view = LayoutInflater.from(context).inflate(R.layout.item_product, parent, false)
+        return ViewHolder(view)
+    }
+
+    //Se le asigna un valor a cada componente
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val product = productList[position]
+        holder.setListener(product)
+        holder.binding.tvName.text = product.name
+        holder.binding.tvPrice.text = product.price.toString()
+        holder.binding.tvQuantity.text = product.price.toString()
+    }
+
+    override fun getItemCount(): Int = productList.size
+
+    fun add(product: Product){
+        if (!productList.contains(product)){
+            productList.add(product)
+            notifyItemInserted(productList.size -1)
+        }
+    }
+
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val binding = ItemProductBinding.bind(view)
 
